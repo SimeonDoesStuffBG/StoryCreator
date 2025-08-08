7d47621b33503708b49a2c867772d7baf26a078c
SimeonDoesStuffBG <simeondoesstuff@gmail.com>
2025-08-08 17:20:07 +0300

    Added character thumbnail and styled it

--- frontend/app/components/characterThumbnail.tsx
+++ frontend/app/components/characterThumbnail.tsx
@@ -2,9 +2,15 @@ import React from "react"
 import { NavLink } from "react-router"
 
 export default function CharacterThumbnail({name, creator}:character){
-    return <NavLink to="/" className={({isActive})=>{return isActive?"charthumbnail":"charthumbnail"}} >
-        <img src="../../public/character.svg" alt="" className="character"/>
-        <h1>{name}</h1>
-        <h3>{creator}</h3>
-    </NavLink>
+    return <NavLink to="/">
+                <div className="charthumbnail">
+                    <img src="/character.svg" alt="" className="picture"/>
+                    <div className="info">
+                        <h1>{name}</h1>
+                        <h3>{creator}</h3>
+                    </div>
+                </div>        
+            </NavLink>
+        
+    
 }//https://www.netjstech.com/2025/06/using-navlink-in-react-router.html
