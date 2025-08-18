274554742169fede95330ce26e4c513687359951
SimeonDoesStuffBG <simeondoesstuff@gmail.com>
2025-08-18 23:18:53 +0300

    Added character thumbnail

---
+++ frontend/src/app/chatacter-thumbnail/chatacter-thumbnail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chatacter-thumbnail',
+  standalone: true,
+  imports: [],
+  templateUrl: './chatacter-thumbnail.component.html',
+  styleUrl: './chatacter-thumbnail.component.scss'
+})
+export class ChatacterThumbnailComponent {
+
+}
