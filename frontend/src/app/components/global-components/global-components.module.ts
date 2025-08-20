0e17cfc9bfef6178de31fc002138faba80ae7b3d
SimeonDoesStuffBG <simeondoesstuff@gmail.com>
2025-08-20 13:59:26 +0300

    Added header component

--- frontend/src/app/components/global-components/global-components.module.ts
+++ frontend/src/app/components/global-components/global-components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from "../../app-routing.module";
 
 @NgModule({
-    declarations:[],
-    imports:[],
-    exports:[]
+    declarations:[HeaderComponent],
+    imports: [AppRoutingModule],
+    exports:[HeaderComponent]
 })
 export default class GLobalComponentsModule{}
